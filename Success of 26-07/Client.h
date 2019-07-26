e796c7cad45e4ffeff5de8e334e714131d04c6a8
HPQuyen <48430116+HPQuyen@users.noreply.github.com>
2019-07-26 21:07:06 +0700

    Success update

---
+++ Success of 26-07/Client.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef Client_h
+#define Client_h
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include "UserAccount.h"
+#include "Date.h"
+#include "Function_Sup.h"
+
+#define MAX_ACCOUNT 10
+
+class Client
+{
+private:
+	string name;
+	string address;
+	string SocialID;
+	string UserID;
+	string Email;
+	vector <UserAccount*> BankAccount;
+	int numOfAccount;
+	Date DoB;
+	bool Sex; // (0:female,1:male)
+	float Salary;
+
+	//	Bank*
+public:
+	Client();
+	//~Client();
+	void setbankaccount(vector <UserAccount*> x) 
+	{
+		this->numOfAccount = x.size();
+		this->BankAccount.swap(x);
+	}
+	Client(string name, string addr, string SocialID, string UserID, string email, Date dob, bool sex, float salary);
+	void input();
+	void output();
+	bool OpenNewAccount();
+	bool CloseAccount();
+	//bool Deposit(float value);
+	//bool WithDraw(float value);
+	//bool Transfer(float value, string AccountID);
+	bool operator ==(const Client *& a);
+	string createPassword();
+	string createUserName();
+
+};
+#endif // !Client_h
